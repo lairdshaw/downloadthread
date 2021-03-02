10e9172fc75741220e57bb2027e1eaeaf93fb998
dragonexpert <arceus@teamdimensional.net>
2021-03-02 15:41:42 -0600

    Issue #6
    Closes #6

---
+++ inc/languages/english/downloadthread.lang.php
@@ -0,0 +1,4 @@
+<?php
+$l['downloadthread_download_disabled'] = "Thread downloading is disabled for this forum.";
+$l['downloadthread_usergroup_no_permission'] = "Your user group does not have permission to download threads.";
+$l['downloadthread_downloadthread'] = "Download Thread";
